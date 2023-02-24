f1316763e514fd298b1596f1e1b92d2b81c96503
kali <sharktallica@gmail.com>
2023-02-24 12:27:20 +0000

    Add search bar placeholder

--- searchform.php
+++ searchform.php
@@ -1,8 +1,13 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
+
     <div>
+
         <label for="s" class="screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
-        <input type="search" id="s" name="s" value="" />
+
+        <input type="search" id="s" name="s" value="" placeholder="<?php _e('Search for:','bonestheme'); ?>" />
 
         <button type="submit" id="searchsubmit" ></button>
+        
     </div>
+
 </form>
